b6b788fa1efbaf0a2c86894b5d99b25ded03f952
James Wang <jw@slickage.com>
2016-11-01 16:03:39 -1000

    routing defaults with stub views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SmartRoutesComponent } from './smart-routes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmartRoutesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
-      {
-      }
+      { path: 'smart-routes', component: SmartRoutesComponent },
+      { path: '', component: AppComponent }
+        // { path: '**', component: PageNotFoundComponent }
     ])
   ],
   providers: [],
---
+++ src/app/smart-routes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './smart-routes.component.html',
+  styleUrls: ['./smart-routes.component.css']
+})
+export class SmartRoutesComponent {
+  title = 'Smart Routes';
+}
+

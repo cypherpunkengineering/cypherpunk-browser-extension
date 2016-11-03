d4b12d25bc7be70d1fbc3663e433e0770b15d6ab
James Wang <jw@slickage.com>
2016-11-02 17:45:37 -1000

    Basic styling for index
    - based on ryan's mockups
    - bourbon based

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,Input, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.scss'],
+  animations: [
+    trigger('slideRight', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('* => void', [
+        animate(1000, style({transform: 'translateX(100%)'}))
+      ]),
+      transition('* => inactive', [
+        animate(1000, style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class IndexComponent {
   title = 'Index';

f4e1e5b4f8880cfc4044ebf73ea317cc84e4fd5b
James Wang <jw@slickage.com>
2016-11-05 19:44:17 -1000

    Moving animation to separate module

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,20 +1,9 @@
-import { Component,Input, trigger, state, style, transition, animate } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss'],
-  animations: [
-    trigger('slideRight', [
-      state('in', style({transform: 'translateX(0)'})),
-      transition('* => void', [
-        animate(1000, style({transform: 'translateX(100%)'}))
-      ]),
-      transition('* => inactive', [
-        animate(1000, style({transform: 'translateX(100%)'}))
-      ])
-    ])
-  ]
+  styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
   title = 'Index';

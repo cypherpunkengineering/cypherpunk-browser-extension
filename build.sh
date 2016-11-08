8c477a59b47a0090835c45837804e405029f7e96
J. Maurice <j@wiz.biz>
2016-11-08 21:56:26 +0900

    Fix jenkins build script

--- build.sh
+++ build.sh
@@ -1,4 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# fetch submodules
+git submodule update --init --recursive
+
+# fix path
+export PATH=$PATH:/usr/local/bin
+
+# prepare nvm
+export NVM_DIR="$HOME/.nvm"
+source "$(brew --prefix nvm)/nvm.sh" v6.8.0
+nvm install v6.8.0
+nvm alias default v6.8.0
+nvm use v6.8.0
+
+# install deps
 npm install
+
+# build extension
 ng build --prod
+
+# package as zip
 npm run compress
+
+# done
+exit 0

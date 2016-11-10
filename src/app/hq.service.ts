44ef310ace905f297ed977651313fe21057a364e
James Wang <jw@slickage.com>
2016-11-10 03:15:40 -1000

    HQ service now parameterizes options
    - enabled debug method to examine session cookie

--- src/app/hq.service.ts
+++ src/app/hq.service.ts
@@ -4,26 +4,29 @@ import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class HqService {
+  options: RequestOptions;
+
   constructor (private http: Http) {
-    // Debug: get this header to check consistency with session token
-    // chrome.cookies.get({url:'https://cypherpunk.engineering', name: 'cypherpunk.session'}, (cookie) => { 
-    //   this.sessionCookie = cookie;
-    // }); 
+    let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
+    this.options = new RequestOptions({ headers: headers, withCredentials: true });
   }
 
   getServerList(): Observable<any> {
-    let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
-    let options = new RequestOptions({ headers: headers, withCredentials: true });
-    return this.http.get('https://cypherpunk.engineering/api/v0/vpn/serverList', options)
+    return this.http.get('https://cypherpunk.engineering/api/v0/vpn/serverList', this.options)
       .map((res:Response) => res.json())
       .catch((error:any) => Observable.throw(error || 'getServerList Error'));
   }
 
   login(): Observable<any>  {
-    let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
-    let options = new RequestOptions({ headers: headers, withCredentials: true });
-    return this.http.post('https://cypherpunk.engineering/api/v0/account/authenticate/userpasswd', '{"login":"test@test.test","password":"test123"}', options)
+    return this.http.post('https://cypherpunk.engineering/api/v0/account/authenticate/userpasswd', '{"login":"test@test.test","password":"test123"}', this.options)
       .map((res:Response) => res.json())
       .catch((error:any) => Observable.throw(error || 'login Error'));
+  }
+
+  debugCheckSession(): void {
+    // Debug: get this header to check consistency with session token
+    chrome.cookies.get({url:'https://cypherpunk.engineering', name: 'cypherpunk.session'}, (cookie) => { 
+      console.log(cookie);
+    }); 
   }
 }

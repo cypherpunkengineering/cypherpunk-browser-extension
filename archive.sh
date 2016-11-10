a429df6610e844ce7f2a5cd4b66197723f2e69d6
J. Maurice <j@wiz.biz>
2016-11-10 23:30:01 +0900

    Fix package archive

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 # archive package artifact
-PKG="dist/*.zip"
-ARTIFACT="../`printf 'cypherpunk-browser-extension-%05d' ${BUILD_NUMBER}`.pkg"
-mv "${PKG}" "${ARTIFACT}"
+PKG="dist/cypherpunk-browser-extension.zip"
+ARTIFACT="`printf 'cypherpunk-browser-extension-%05d' ${BUILD_NUMBER}`.zip"
+cp "${PKG}" "${ARTIFACT}"
 scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
 
 exit 0

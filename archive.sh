262673d657d67fcf198e4817b028f53b50898ab4
J. Maurice <j@wiz.biz>
2016-11-18 22:05:49 +0900

    Rename package name to cypherpunk-privacy-chrome

--- archive.sh
+++ archive.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # archive package artifact
-PKG="dist/cypherpunk-browser-extension.zip"
-ARTIFACT="`printf 'cypherpunk-browser-extension-%05d' ${BUILD_NUMBER}`.zip"
+PKG="dist/cypherpunk-privacy-chrome.zip"
+ARTIFACT="`printf 'cypherpunk-privacy-chrome-%05d' ${BUILD_NUMBER}`.zip"
 cp "${PKG}" "${ARTIFACT}"
 scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
 

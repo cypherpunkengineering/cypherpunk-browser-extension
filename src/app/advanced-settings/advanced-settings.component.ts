18ada19f0f005724988c8352d17722ca0eec7abe
Anthony Kinsey <anthony@slickage.com>
2016-11-18 16:13:26 -1000

    Initial layout of the advanced settings view

--- src/app/advanced-settings/advanced-settings.component.ts
+++ src/app/advanced-settings/advanced-settings.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './advanced-settings.component.html',
-  styleUrls: ['./advanced-settings.component.css']
+  styleUrls: ['./advanced-settings.component.scss']
 })
 export class AdvancedSettingsComponent {
   title = 'Advanced Settings';

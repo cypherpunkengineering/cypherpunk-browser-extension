922a60187f451e14718be4ccfd7f8fec87e8cc1a
Anthony Kinsey <anthony@slickage.com>
2017-02-06 16:20:14 -1000

    Firefox css fixes. Firefox build script
    * Fixed scrollbars and background issue with firefox
    
    * Added production/dev to the firefox build script
      --prod flag builds for production and outputs xpi
      no flag builds for dev

--- firefox-build.sh
+++ firefox-build.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+# copy over firefox.angular-cli.json to angular-cli.json
 mv angular-cli.json chrome.angular-cli.json
 mv firefox.angular-cli.json angular-cli.json
 
-ng build
+# rename firefox.manifest.json to manifest.json
+mv src/manifest.json src/chrome.manifest.json
+mv src/firefox.manifest.json src/manifest.json
 
+# build extension
+if [[ $* == *--prod* ]]
+then
+  echo "Building for production"
+  ng build --aot
+else
+  echo "Building for development"
+  ng build
+fi
+
+# move original angular-cli.json back
 mv angular-cli.json firefox.angular-cli.json
 mv chrome.angular-cli.json angular-cli.json
 
+# move original manifest.json back
+mv src/manifest.json src/firefox.manifest.json
+mv src/chrome.manifest.json src/manifest.json
+
+# copy to dist folder
 cp -R firefox-entrypoint/* dist-firefox
-mv dist-firefox/webextension/firefox.manifest.json dist-firefox/webextension/manifest.json
+
+# package as xpi
+if [[ $* == *--prod* ]]
+then
+  npm run compress-ff
+fi
+
+# done
+exit 0

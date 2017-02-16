6639e7c0b37a3ce4b772695814999d94601e58e2
J. Maurice <j@wiz.biz>
2017-02-16 16:15:35 +0900

    Upload build artifacts to GCS bucket

--- archive-chrome.sh
+++ archive-chrome.sh
@@ -4,6 +4,9 @@
 PKG="dist/cypherpunk-privacy-chrome.zip"
 ARTIFACT="`printf 'cypherpunk-privacy-chrome-%05d' ${BUILD_NUMBER}`.zip"
 cp "${PKG}" "${ARTIFACT}"
+echo "Uploading build to builds repo..."
 scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
+echo "Uploading build to GCS bucket..."
+gsutil cp "${ARTIFACT}" gs://builds.cypherpunk.com/builds/chrome/
 
 exit 0
--- archive-firefox.sh
+++ archive-firefox.sh
@@ -4,6 +4,9 @@
 PKG="dist-firefox/cypherpunk-privacy-firefox.xpi"
 ARTIFACT="`printf 'cypherpunk-privacy-firefox-%05d' ${BUILD_NUMBER}`.xpi"
 cp "${PKG}" "${ARTIFACT}"
+echo "Uploading build to builds repo..."
 scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
+echo "Uploading build to GCS bucket..."
+gsutil cp "${ARTIFACT}" gs://builds.cypherpunk.com/builds/firefox/
 
 exit 0

267675d277e09294b80410ce6f6c654077a8de89
Edward Kim <ed@slickage.com>
2017-02-15 17:59:45 -1000

    Update build scripts

--- archive-firefox.sh
+++ archive-firefox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # archive package artifact
-PKG="dist/cypherpunk-privacy-firefox.zip"
-ARTIFACT="`printf 'cypherpunk-privacy-firefox-%05d' ${BUILD_NUMBER}`.zip"
+PKG="dist-firefox/cypherpunk-privacy-firefox.xpi"
+ARTIFACT="`printf 'cypherpunk-privacy-firefox-%05d' ${BUILD_NUMBER}`.xpi"
 cp "${PKG}" "${ARTIFACT}"
 scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
 
--- build-firefox.sh
+++ build-firefox.sh
@@ -2,32 +2,32 @@
 set -e
 echo "Building for Firefox production"
 
-# # fetch submodules
-# git submodule update --init --recursive
-#
-# # fix path
-# export PATH=$PATH:/usr/local/bin
-#
-# # build vars
-# NODE_VERSION=v6.9.3
-#
-# # load nvm depending on OS
-# export NVM_DIR=$HOME/.nvm
-# case `uname -s` in
-# 	Darwin)
-# 		source "$(brew --prefix nvm)/nvm.sh"
-# 		;;
-# 	FreeBSD|Linux)
-# 		source $HOME/.nvm/nvm.sh
-# 		;;
-# esac
-#
-# # prepare node/npm
-# nvm install ${NODE_VERSION}
-# nvm use ${NODE_VERSION}
-#
-# # install deps
-# npm install
+# fetch submodules
+git submodule update --init --recursive
+
+# fix path
+export PATH=$PATH:/usr/local/bin
+
+# build vars
+NODE_VERSION=v6.9.3
+
+# load nvm depending on OS
+export NVM_DIR=$HOME/.nvm
+case `uname -s` in
+	Darwin)
+		source "$(brew --prefix nvm)/nvm.sh"
+		;;
+	FreeBSD|Linux)
+		source $HOME/.nvm/nvm.sh
+		;;
+esac
+
+# prepare node/npm
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
+
+# install deps
+npm install
 
 # copy over firefox.angular-cli.json to angular-cli.json
 cp firefox.angular-cli.json angular-cli.json

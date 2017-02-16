a2d442561056a583a18ee2d1754ad9e70f83c60b
J. Maurice <j@wiz.biz>
2017-02-16 20:36:17 +0900

    Update firefox webhook URL

--- archive-firefox.sh
+++ archive-firefox.sh
@@ -9,6 +9,6 @@ scp -P92 "${ARTIFACT}" upload@builds-upload.cypherpunk.engineering:/data/builds/
 echo "Uploading build to GCS bucket..."
 gsutil cp "${ARTIFACT}" gs://builds.cypherpunk.com/builds/firefox/
 echo "Sending notification to slack..."
-curl -X POST --data "payload={\"text\": \"cypherpunk-privacy-firefox build ${BUILD_NUMBER} is now available from https://download.cypherpunk.com/builds/firefox/${ARTIFACT}\"}" https://hooks.slack.com/services/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
+curl -X POST --data "payload={\"text\": \"cypherpunk-privacy-firefox build ${BUILD_NUMBER} is now available from https://download.cypherpunk.com/builds/firefox/${ARTIFACT}\"}" https://hooks.slack.com/services/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
 
 exit 0

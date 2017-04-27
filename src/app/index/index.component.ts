720c6bb7fad83fb0641efa682c2e6c7726f026a5
Edward Kim <ed@slickage.com>
2017-04-26 17:21:26 -1000

    World map on main page

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -2,7 +2,7 @@ import { Router } from '@angular/router';
 import { HqService } from '../hq.service';
 import { SettingsService } from '../settings.service';
 import { ProxySettingsService } from '../proxy-settings.service';
-import { Component, ViewChild, style, animate, transition, state, trigger, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +20,6 @@ export class IndexComponent implements AfterViewChecked {
   cypherpunkEnabled: boolean;
 
   // display Vars
-  currentServer: {};
   serverId: string;
   serverName: string;
   serverFlag: string;
@@ -28,6 +27,18 @@ export class IndexComponent implements AfterViewChecked {
   connectionStatus = 'Disconnected';
   regions = {}; // used in the view
   servers = [];
+
+  // Map Vars
+  pi = Math.PI;
+  halfPi = this.pi / 2;
+  epsilon = Number.EPSILON;
+  currentServer;
+  hoverServer;
+  cypherplayMarker = {
+    scale: 0.5,
+    mapX: 939.232,
+    mapY: 530.757
+  };
 
   constructor(
     private router: Router,
@@ -45,13 +56,25 @@ export class IndexComponent implements AfterViewChecked {
     this.serverFlag = this.settingsService.serverFlag;
     this.serverLevel = this.settingsService.serverLevel;
     this.accountType = this.settingsService.accountType;
+
+    // Check for Cypherplay set as default
     if (!this.serverId) {
       this.serverName = 'Cypherplay™';
       this.serverFlag = '/assets/icon_cypherplay@2x.png';
     }
-    if (this.cypherpunkEnabled) {
-      this.connectionStatus = 'Connected';
-    }
+
+    // Double check connection status display
+    if (this.cypherpunkEnabled) { this.connectionStatus = 'Connected'; }
+
+    // generate XY points for all servers
+    this.translateLocations(this.servers);
+    // Set current Server for mapping
+    this.servers.map((server) => {
+      if (server.id === this.serverId) { this.currentServer = server; }
+    });
+
+    // if current server not set, use cypherplay
+    if (!this.currentServer) { this.currentServer = this.cypherplayMarker; }
 
     // manage other extensions
     this.proxySettingsService.proxyExtObservable.subscribe(
@@ -108,6 +131,57 @@ export class IndexComponent implements AfterViewChecked {
     let url = 'chrome://settings';
     chrome.tabs.create({ url: url });
   }
+
+  vanDerGrinten3Raw(lambda, phi) {
+    if (Math.abs(phi) < this.epsilon) { return [lambda, 0]; }
+
+    let sinTheta = phi / this.halfPi;
+    let theta = Math.asin(sinTheta);
+
+    if (Math.abs(lambda) < this.epsilon || Math.abs(Math.abs(phi) - this.halfPi) < this.epsilon) {
+      return [0, this.pi * Math.tan(theta / 2)];
+    }
+
+    let A = (this.pi / lambda - lambda / this.pi) / 2;
+    let y1 = sinTheta / (1 + Math.cos(theta));
+
+    return [
+      this.pi * (Math.sign(lambda) * Math.sqrt(A * A + 1 - y1 * y1) - A),
+      this.pi * y1
+    ];
+  }
+
+  transformToXY(lat, long) {
+    let coords = this.vanDerGrinten3Raw((long - 11) * this.pi / 180, lat * this.pi / 180);
+    coords[0] = (coords[0] * 150 + (920 / 2)) * (2000 / 920);
+    coords[1] = (coords[1] * 150 + (500 / 2 + 500 * 0.15)) * (2000 / 920);
+    return coords;
+  }
+
+  translateLocations(servers) {
+    servers.forEach((server) => {
+      if (!server.lat || !server.lon) { return; }
+      let [ x, y ] = this.transformToXY(server.lat, server.lon);
+      server.mapX = x;
+      server.mapY = y;
+      server.scale = server.scale || 1;
+    });
+    return servers;
+  }
+
+  generateMapStyle() {
+    if (this.showLocationList && this.hoverServer) {
+      return { 'transform': `scale(${this.hoverServer.scale}) translate(-${this.hoverServer.mapX}px, -${this.hoverServer.mapY}px)`};
+    }
+    else if (!this.showLocationList && this.currentServer) {
+      return { 'transform': `scale(${this.currentServer.scale}) translate(-${this.currentServer.mapX}px, -${this.currentServer.mapY}px)`};
+    }
+    else {
+      return { 'transform': `scale(${this.cypherplayMarker.scale}) translate(-${this.cypherplayMarker.mapX}px, -${this.cypherplayMarker.mapY}px)`};
+    }
+  }
+
+  setHoverServer(server) { this.hoverServer = server; }
 
   tutorialVisible(visible: boolean) { this.showTutorial = visible; }
 
@@ -168,12 +242,16 @@ export class IndexComponent implements AfterViewChecked {
       this.serverLevel = '';
       this.serverName = 'Cypherplay™';
       this.serverFlag = '/assets/icon_cypherplay@2x.png';
+      this.currentServer = this.cypherplayMarker;
+      this.hoverServer = this.cypherplayMarker;
     }
     else {
       this.serverId = server.id;
       this.serverName = server.name;
       this.serverLevel = server.level;
       this.serverFlag = `/assets/flags/24/${server.country}.png`;
+      this.currentServer = server;
+      this.hoverServer = server;
     }
 
     // proxy related variables
